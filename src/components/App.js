fe2ea8bb4186e15a1557b2e11377f55b8684cef5
psatler <pablosatler@gmail.com>
2018-10-12 13:48:58 -0300

    feat: create routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import Navigation from "./Navigation";
-
-import logo from "./logo.svg";
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import logo from "./logo.svg";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <BrowserRouter>
-          <Navigation />
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+import * as routes from "../constants/routes";
+
+//nav stuff
+import Navigation from "./Navigation";
+import LandingPage from "./Landing";
+import SignUpPage from "./SignUp";
+import SignInPage from "./SignIn";
+import PasswordForgetPage from "./PasswordForget";
+import HomePage from "./Home";
+import AccountPage from "./Account";
+
+const App = () => (
+  // <div className="App">
+  <BrowserRouter>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={routes.LANDING} component={LandingPage} />
+      <Route exact path={routes.SIGN_UP} component={SignUpPage} />
+      <Route exact path={routes.SIGN_IN} component={SignInPage} />
+      <Route
+        exact
+        path={routes.PASSWORD_FORGET}
+        component={PasswordForgetPage}
+      />
+      <Route exact path={routes.HOME} component={HomePage} />
+      <Route exact path={routes.ACCOUNT} component={AccountPage} />
+    </div>
+  </BrowserRouter>
+  // </div>
+);
 
 export default App;
