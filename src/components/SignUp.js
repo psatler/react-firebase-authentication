89995e031d10c49895972d34811f192e013e5bc2
psatler <pablosatler@gmail.com>
2018-10-12 19:42:42 -0300

    feat: add signup file

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,117 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
 
-const SignUpPage = () => (
+import * as routes from "../constants/routes";
+import { auth } from "../firebase";
+
+const SignUpPage = ({ history }) => (
   <div>
-    <h1>SignUpPage</h1>
+    <h1>SignUp</h1>
+    <SignUpForm history={history} />
   </div>
 );
 
-export default SignUpPage;
+//################### Sign Up Form ###################
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
+};
+
+//A Higher order function with prop name as key and the value to be assigned to
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value
+});
+
+class SignUpForm extends Component {
+  //defining state
+  state = {
+    ...INITIAL_STATE
+  };
+
+  // onChange = (propName, value) => {
+  //   this.setState({
+  //     [propName]: value
+  //   });
+  // };
+
+  onSubmit = event => {
+    const { username, email, passwordOne } = this.state;
+    const { history } = this.props;
+    auth
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      //it the above functions resolves, reset the state to its initial state values, otherwise, set the error object
+      .then(authUser => {
+        this.setState({
+          ...INITIAL_STATE
+        });
+        history.push(routes.HOME); //redirects to Home Page
+      })
+      .catch(err => {
+        this.setState(byPropKey("error", err));
+      });
+
+    event.preventDefault(); //prevents refreshing
+  };
+
+  render() {
+    const { username, email, passwordOne, passwordTwo, error } = this.state;
+    //a boolen to perform validation
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === "" ||
+      email === "" ||
+      username === "";
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={username}
+          onChange={e => this.setState(byPropKey("username", e.target.value))}
+          // onChange={e => this.onChange("username", e.target.value)}
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          value={email}
+          onChange={e => this.setState(byPropKey("email", e.target.value))}
+          type="text"
+          placeholder="Email Address"
+        />
+        <input
+          value={passwordOne}
+          onChange={e =>
+            this.setState(byPropKey("passwordOne", e.target.value))
+          }
+          type="password"
+          placeholder="Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={e =>
+            this.setState(byPropKey("passwordTwo", e.target.value))
+          }
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Sign Up
+        </button>
+
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const SignUpLink = () => (
+  <p>
+    Don't have an account? <Link to={routes.SIGN_UP}>Sign Up</Link>
+  </p>
+);
+
+//exports
+export default withRouter(SignUpPage); //using a HoC to get access to history
+export { SignUpForm, SignUpLink };
