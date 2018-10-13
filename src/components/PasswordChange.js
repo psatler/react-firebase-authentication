f7a15f17211f95f3f64c35e7446a0856902d10f3
psatler <pablosatler@gmail.com>
2018-10-13 00:01:55 -0300

    fix: change button text

--- src/components/PasswordChange.js
+++ src/components/PasswordChange.js
@@ -54,7 +54,7 @@ class PasswordChangeForm extends Component {
           placeholder="Confirm New Password"
         />
         <button disabled={isInvalid} type="submit">
-          Reset My Password
+          Change My Password
         </button>
 
         {error && <p>{error.message}</p>}
