66fb8bcddd6006b32ffc67416363127e9f59963e
psatler <pablosatler@gmail.com>
2018-10-13 12:46:58 -0300

    feat: show list of users stored in db

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
 import withAuthorization from "./withAuthorization";
+import { db } from "../firebase";
 
-const HomePage = () => (
+class HomePage extends Component {
+  state = {
+    users: null
+  };
+
+  componentDidMount() {
+    db.onceGetUsers().then(res => {
+      this.setState({
+        users: res.val()
+      });
+    });
+  }
+
+  render() {
+    const { users } = this.state;
+
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>The Home Page is accessible by every signed in user.</p>
+        {!!users && <UserList users={users} />}
+      </div>
+    );
+  }
+}
+
+const UserList = ({ users }) => (
   <div>
-    <h1>Home Page</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
+    <h2>List of Usernames of Users</h2>
+    <p>(Saved on Sign Up in Firebase Database)</p>
+
+    {Object.keys(users).map(key => (
+      <div key={key}>{users[key].username}</div>
+    ))}
   </div>
 );
 
 const authCondition = authUser => !!authUser;
 
-export default withAuthorization(authCondition)(HomePage);
+export default withAuthorization(authCondition)(HomePage); //grants authorization to open endpoint if an user is signed in
 
 //# admin page role example
 // const AdminPage = () =>
