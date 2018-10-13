6f871a514a0c3876bab5ab9db79cc5ce4ea6f625
psatler <pablosatler@gmail.com>
2018-10-13 18:48:29 -0300

    style: add lorem ipsum to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,17 @@ import React from "react";
 
 const LandingPage = () => (
   <div>
-    <h1>LandingPage</h1>
+    <h1>Landing Page</h1>
+
+    <p>This page is public, accessible by everyone</p>
+
+    <p>
+      Est cillum sunt qui nulla esse mollit quis magna enim non non laborum
+      culpa nisi. Amet do nisi minim amet dolor quis veniam fugiat exercitation
+      duis anim occaecat. Mollit pariatur minim aute eiusmod est ad dolore
+      labore fugiat deserunt quis. Aliquip dolor ex irure sunt voluptate
+      exercitation voluptate incididunt.
+    </p>
   </div>
 );
 
